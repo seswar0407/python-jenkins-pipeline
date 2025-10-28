d5df0fb465c28e3534db283d18d47aa3c2ad303e
Ubuntu <ubuntu@ip-172-31-25-148.ap-southeast-1.compute.internal>
2025-10-28 06:53:37 +0000

    python_jenkins_file

---
+++ calc.py
@@ -0,0 +1,10 @@
+def add(a,b):
+  return a + b
+
+def multiply(a,b):
+  return a * b
+
+if __name__ =="__main__":
+  x,y = 5,3
+  print("Addition:",add(x,y))
+  print("Multiplication:",multiply(x,y))
