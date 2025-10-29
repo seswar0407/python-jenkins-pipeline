c174794808fd6114ef8ac2af461d9a83201fec5b
Ubuntu <ubuntu@ip-172-31-25-148.ap-southeast-1.compute.internal>
2025-10-29 03:05:37 +0000

    updated Auto trigger test

--- calc.py
+++ calc.py
@@ -4,9 +4,14 @@ def add(a,b):
 def multiply(a,b):
   return a * b
 
+def divison(a,b):
+  return a / b
+
 if __name__ =="__main__":
   x,y = 5,3
   print("Addition:",add(x,y))
   print("Multiplication:",multiply(x,y))
+  print("Divison:",divison(x,y))
 
-print("Triggered Automatically fro GitHub...!")
+print("Triggered Automatically from GitHub...!")
+print("Thanks for Triggering by automatically")
