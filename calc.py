5e0ca73b543ebf8fb9a900932cf08a456b00854f
Ubuntu <ubuntu@ip-172-31-25-148.ap-southeast-1.compute.internal>
2025-10-29 03:16:54 +0000

    Updated Auto trigger test1

--- calc.py
+++ calc.py
@@ -7,9 +7,13 @@ def multiply(a,b):
 def divison(a,b):
   return a / b
 
+def subtraction(a,b):
+  return a - b
+
 if __name__ =="__main__":
   x,y = 5,3
   print("Addition:",add(x,y))
+  print("Subtraction",subtraction(x,y))
   print("Multiplication:",multiply(x,y))
   print("Divison:",divison(x,y))
 
